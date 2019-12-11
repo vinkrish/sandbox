4c9b4c97657f70b0c4fa842dd983563c0a288ea2
Vinay Krishna <vinaykrishna1989@gmail.com>
2019-12-11 17:00:17 -0500

    updated js content

--- JavaScript/es6/in NodeJS/destructuring.js
+++ JavaScript/es6/in NodeJS/destructuring.js
@@ -16,3 +16,32 @@ function greetingService(name){
 var {sayHi:hi} = greetingService('Jon');
 
 hi();
+
+// Examples:
+
+function getValues() {
+    return [getFirstValue(), getSecondValue()];
+}
+
+// to access them
+var values = getValues();
+var first = values[0];
+var second = values[1];
+
+// es6 syntax
+const [first, second] = getValues();
+
+function getValues() {
+    return {
+        first: getFirstValue(),
+        second: getSecondValue(),
+    };
+}
+
+// to access them
+var values = getValues();
+var first = values.first;
+var second = values.second;
+
+// es6 syntax
+const {first, second} = getValues();
